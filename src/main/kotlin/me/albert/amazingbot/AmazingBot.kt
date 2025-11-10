cd0ba3f5fef68ce7fe2ffbbff1e8d0e788e02d18
albert4719 <mcdoeswhat@qq.com>
2025-11-10 17:16:04 +0800

    Initial commit

---
+++ src/main/kotlin/me/albert/amazingbot/AmazingBot.kt
@@ -0,0 +1,14 @@
+package me.albert.amazingbot
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AmazingBot : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
