03435aec321362eea99e579aad82de818df8c5af
albert4719 <mcdoeswhat@qq.com>
2025-11-10 17:53:44 +0800

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,15 @@ repositories {
     maven("https://repo.papermc.io/repository/maven-public/") {
         name = "papermc-repo"
     }
+    maven("https://repo.tcoded.com/releases") {
+        name = "tcoded-releases"
+    }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.tcoded:FoliaLib:0.5.1")
     implementation("org.java-websocket:Java-WebSocket:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
@@ -29,6 +33,11 @@ tasks {
         // Your plugin's jar (or shadowJar if present) will be used automatically.
         minecraftVersion("1.21")
     }
+    shadowJar {
+        relocate("com.tcoded.folialib", "me.albert.amazingbot.libs.folialib")
+        relocate("org.java_websocket", "me.albert.amazingbot.libs.websocket")
+
+    }
 }
 
 val targetJavaVersion = 21
@@ -39,6 +48,7 @@ kotlin {
 tasks.build {
     dependsOn("shadowJar")
 }
+
 
 tasks.processResources {
     val props = mapOf("version" to version)
